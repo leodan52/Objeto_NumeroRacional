c5b30143354aa943a342a59502b20a1d2a38d06e
Leonardo D. Santiago <leodan.52@gmail.com>
2023-04-30 16:52:57 -0600

    Encapsulando atributos y métodos. Creando metodo para invertir. Actualizando método __SubStr_repetida usando regex

--- TOOLS/fraccion.py
+++ TOOLS/fraccion.py
@@ -1,34 +1,35 @@
 # Descripcion
 
-
+import re
 
 class Fraccion:
 
 	def __init__ (self, *arg):
 
 		if arg[0] == 0:
-			self.signo = 1
-			self.vector = [0,1,1]
+			self.__signo = 1
+			self.__vector = [0,1,1]
+			self.__valornumerico = 0.0
 		elif len(arg) == 2:
 			numerador, denominador = arg[0], arg[1]
-			self.valornumerico = numerador/denominador
-			self.signo = int(self.valornumerico / abs(self.valornumerico))
-			self.vector = [abs(numerador), abs(denominador), self.signo]
+			self.__valornumerico = numerador/denominador
+			self.__signo = int(self.__valornumerico / abs(self.__valornumerico))
+			self.__vector = [abs(numerador), abs(denominador), self.__signo]
 		elif len(arg) == 1:
-			self.valornumerico = arg[0]
-			if not isinstance(self.valornumerico, int) and not isinstance(self.valornumerico, float):
-				raise TypeError(f'Entrada inválida, debe ser int o float. Es {type(self.valornumerico)}')
-			elif isinstance(self.valornumerico, int):
-				self.signo = int(self.valornumerico / abs(self.valornumerico))
-				self.vector = [abs(self.valornumerico), 1, self.signo]
+			self.__valornumerico = arg[0]
+			if not isinstance(self.__valornumerico, int) and not isinstance(self.__valornumerico, float):
+				raise TypeError(f'Entrada inválida, debe ser int o float. Es {type(self.__valornumerico)}')
+			elif isinstance(self.__valornumerico, int):
+				self.__signo = int(self.__valornumerico / abs(self.__valornumerico))
+				self.__vector = [abs(self.__valornumerico), 1, self.__signo]
 			else:
-				self.signo = int(self.valornumerico / abs(self.valornumerico))
-				self.vector = Fraccion.Decimal2Fraccion(abs(self.valornumerico)) + [self.signo]
+				self.__signo = int(self.__valornumerico / abs(self.__valornumerico))
+				self.__vector = Fraccion.__DecimalFinito(abs(self.__valornumerico)) + [self.__signo]
 
-		self.Simplificar()
+		self.__Simplificar()
 
 	def __repr__(self):
-		numerador, denominador, signo = self.vector
+		numerador, denominador, signo = self.__vector
 
 		if numerador == 0:
 			return "0"
@@ -38,119 +39,127 @@ class Fraccion:
 			return f'{signo*numerador}:{denominador}'
 
 
-	def Simplificar(self):
+	def __Simplificar(self):
 
-		if self.vector[0] == 0:
-			self.vector = [0,1,1]
+		if self.__vector[0] == 0:
+			self.__vector = [0,1,1]
 			return
 
-		a = self.vector[0]
-		b = self.vector[1]
-		Algunos, todos = Fraccion.Factorizacion_prima(a, b)
+		a = self.__vector[0]
+		b = self.__vector[1]
+		Algunos, todos = Fraccion.__Factorizacion_prima(a, b)
 
 		MCD = 1
 
 		for primo in todos:
 			MCD *= primo
 
-		self.vector[0] = a // MCD
-		self.vector[1] = b // MCD
+		self.__vector[0] = a // MCD
+		self.__vector[1] = b // MCD
 
 
 	def getNumerador(self):
-		return self.vector[0]
+		return self.__vector[2]*self.__vector[0]
 
 	def getDenominador(self):
-		return self.vector[1]
+		return self.__vector[1]
 
-	def getSigno(self):
-		return self.vector[2]
+	def getValorNumerico(self):
+		return self.__valornumerico
 
 
 	def __add__(self, other):
 		if not isinstance(other, Fraccion):
-			other = Fraccion.Clase(other)
+			other = Fraccion.__Clase(other)
 
-		algunos, todos = Fraccion.Factorizacion_prima(self.vector[1], other.vector[1])
+		algunos, todos = Fraccion.__Factorizacion_prima(self.__vector[1], other.__vector[1])
 		mcm = 1
 
 		for i in algunos:
 			mcm *= i
 
-		a = self.signo*self.vector[0]*(mcm//self.vector[1])
-		b = other.signo*other.vector[0]*(mcm//other.vector[1])
+		a = self.__signo*self.__vector[0]*(mcm//self.__vector[1])
+		b = other.__signo*other.__vector[0]*(mcm//other.__vector[1])
 
 		nuevo_denominador = a + b
 
-		return Fraccion.Clase(nuevo_denominador, mcm)
+		return Fraccion.__Clase(nuevo_denominador, mcm)
 
 	def __neg__(self):
-		return Fraccion.Clase(-self.signo*self.vector[0], self.vector[1])
+		return Fraccion.__Clase(-self.__signo*self.__vector[0], self.__vector[1])
 
 	def __sub__(self, other):
-		return Fraccion.Resta(self, other)
+		return Fraccion.__Resta(self, other)
 
 	def __mul__(self, other):
 		if not isinstance(other, Fraccion):
-			other = Fraccion.Clase(other)
+			other = Fraccion.__Clase(other)
 
 		salida = []
 		for i in range(3):
-			salida.append(self.vector[i]*other.vector[i])
+			salida.append(self.__vector[i]*other.__vector[i])
 
 		salida = [salida[2]*salida[0], salida[1]]
 
-		return Fraccion.Clase(*salida)
+		return Fraccion.__Clase(*salida)
 
 	def __truediv__(self, other):
 		if not isinstance(other, Fraccion):
-			other = Fraccion.Clase(other)
+			other = Fraccion.__Clase(other)
 
-		return Fraccion.Division(self, other)
+		return Fraccion.__Division(self, other)
+
+	def __rtruediv__(self, other):
+		if not isinstance(other, Fraccion):
+			other = Fraccion.__Clase(other)
+
+		return Fraccion.__Division(other, self)
 
 	def __pow__(self, other):
-		a = self.vector[0]
-		b = self.vector[1]
+		a = self.__vector[0]
+		b = self.__vector[1]
 
 		if not isinstance(other, Fraccion):
 			other = Fraccion(other)
 
-		if other.vector[0] % 2 != 0 and self.vector[2] == -1:
+		if other.__vector[0] % 2 != 0 and self.__vector[2] == -1:
 			signo = -1
 		else:
 			signo = 1
 
+		a = (a**other.__vector[0])**(1/other.__vector[1])
+		b = (b**other.__vector[0])**(1/other.__vector[1])
 
-		a = (a**other.vector[0])**(1/other.vector[1])
-		b = (b**other.vector[0])**(1/other.vector[1])
-
-		if other.vector[2] == -1:
+		if other.__vector[2] == -1:
 			a, b = b, a
 
 		if int(a) - a != 0 or int(a) - a != 0:
-			return Fraccion.Clase(signo*a/b)
+			return Fraccion.__Clase(signo*a/b)
 		else:
 			a, b = int(a), int(b)
 
-		return Fraccion.Clase_3arg(a,b,signo)
+		return Fraccion.__Clase_3arg(a,b,signo)
+
+	def invertir(self):
+		return 1 / self
 
 
 	@classmethod
-	def Clase(cls, *arg):
+	def __Clase(cls, *arg):
 		return cls(*arg)
 
 	@classmethod
-	def Clase_3arg(cls, *arg):
+	def __Clase_3arg(cls, *arg):
 		a = arg[0]*arg[2]
 		b = arg[1]
 		return cls(a,b)
 
 	@staticmethod
-	def Resta(a, b):
+	def __Resta(a, b):
 		return a + (-b)
 
 	@staticmethod
-	def Division(a, b):
+	def __Division(a, b):
 		return a * b**-1
 
 	@classmethod
@@ -166,34 +175,28 @@ class Fraccion:
 			signo = 1
 		str_numero = str(Numero).split(".")
 
-
-
 		if int(str_numero[1]) == 0:
 			return cls(Parte_entera, 1, signo)
 
-		patron, veces = Fraccion.SubStr_repetida(str_numero[1])
-		if veces == 1:
+		patron, grupo0 = Fraccion.__SubStr_repetida(str_numero[1])
+		if patron == "":
 			patron = str_numero[1]
-	#		return cls(*Fraccion.Decimal2Fraccion(Numero))
+	#		return cls(*Fraccion.__DecimalFinito(Numero))
 
-		longitud_repetida = len(patron*veces)
-		aux = list(str_numero[1])
-		del aux[-longitud_repetida:]
-		str_decimal = "".join(aux)
-		longitud_decimal = len(str_decimal)
+		str_decimal = re.sub(grupo0 + "$", "", str_numero[1])
 		if str_decimal == "":
 			str_decimal = "0"
 
 		fraccion_entero = cls(signo*Parte_entera)
 		fraccion_decimal1 = cls(signo*int(str_decimal), 10**(len(str_decimal)))
 		denominador_ = "9"*len(patron)
-		fraccion_decimal2 = cls(signo*int(patron), int(denominador_)*10**longitud_decimal )
+		fraccion_decimal2 = cls(signo*int(patron), int(denominador_)*10**(len(str_decimal)))
 
 		return fraccion_entero + fraccion_decimal1 + fraccion_decimal2
 
 
 	@staticmethod
-	def Decimal2Fraccion(Numero):
+	def __DecimalFinito(Numero):
 		Parte_entera = int(Numero)
 		str_numero = str(Numero)
 
@@ -204,7 +207,7 @@ class Fraccion:
 
 
 	@staticmethod
-	def Factorizacion_prima(*arg):
+	def __Factorizacion_prima(*arg):
 
 		Lista = [*arg]
 		for i in range(len(Lista)):
@@ -254,36 +257,12 @@ class Fraccion:
 		return tuple(divide_alguno), tuple(divide_todos)
 
 	@staticmethod
-	def SubStr_repetida(cadena):
-		largo = len(cadena)
-		maximo = largo//2 + 1
-		len_patron = 1
-		Mas_repite = 1
-		salida = "Nada"
-
-		subcadenas = dict()
-
-		for m in range(1,largo):
-			substr = cadena[-m:]
-			subcadenas[substr] = 0
-
-			if m >= maximo+1:
-				break
-
-			for n in range(m, largo+1, m):
-				check = cadena[-n:]
-				factor = n//m
-				if substr*factor == check:
-					subcadenas[substr] += 1
-				else:
-					break
-
-		for sub in subcadenas:
-			if subcadenas[sub] > Mas_repite:
-				Mas_repite = subcadenas[sub]
-				salida = sub
-
-		return salida, Mas_repite
+	def __SubStr_repetida(cadena):
+		search_ = re.search(r'(\d+)\1{2,}\Z', cadena)
+		try:
+			return search_.group(1), search_.group(0)
+		except AttributeError:
+			return "", ""
 
 
 class Divisivilidad:
@@ -328,5 +307,5 @@ class Divisivilidad:
 
 
 	@classmethod
-	def Clase(cls,tupla):
+	def __Clase(cls,tupla):
 		return cls(*tupla)
